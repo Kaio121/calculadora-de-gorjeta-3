d6286883470bcc71c370623b5acaef06293f20f1
kaio rocha <kaio653@icloud.com>
2024-04-01 21:57:29 -0300

    nono commit do CSS e terceiro commit do JS

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 let conta = 0
+let pessoas = 0
 
 const contaInput = document.querySelector("#conta")
-contaInput.addEventListener("input")
+contaInput.addEventListener("input", receberValorConta)
 
-function receberValorConta() {
-    console.log(evento)
+function receberValorConta(evento) {
+    conta = Number(evento.target.value)
+}
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQuantidadePessoas)
+
+function receberQuantidadePessoas (evento) {
+    const paragrafoErro = document.querySelector(".pessoas #erro")
+    const divErro = document.querySelector(".pessoas .input-box")
+
+    if(evento.target.value === "0") {
+        paragrafoErro.style.display = "block"
+        divErro.setAttribute("id", "erro-div")
+    } else {
+        paragrafoErro.style.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas = Number(evento.target.value)
+    }
+}
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+console.log(botoesGorjeta)
+    botoesGorjeta.forEach(botao =>{
+        botao.addEventListener("click", receberPorcentagemBotao)
+})
+
+function receberPorcentagem() {
+        console.log("chamou")
+}
+
+function receberPorcentagemBotao(evento) {
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+
+        if(botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+    porcetagem = parseFloat(evento.target.value) / 100
 }
